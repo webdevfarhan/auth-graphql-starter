bc20dfb215913740d89a391ffb549d8b57b33e6c
Farhan <frhnmdni@gmail.com>
2019-08-22 23:22:33 +0530

    binding query to header component

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import query from '../queries/CurrentUser';
 
 class Header extends Component {
   render() {
+    console.log(this.props.data);
     return <div>Header</div>;
   }
 }
 
-export default Header;
+export default graphql(query)(Header);
