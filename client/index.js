54c08876b2e2ef7df82b08a4390ffc2ee03432c9
Farhan <frhnmdni@gmail.com>
2019-09-02 13:14:07 +0530

    auth form

--- client/index.js
+++ client/index.js
@@ -15,8 +15,9 @@ const Root = () => {
   return (
     <ApolloProvider client={client}>
       <Router history={hashHistory}>
-        <Route path='/' component={App} />
-        <Route path='/login' component={LoginForm} />
+        <Route path='/' component={App}>
+          <Route path='/login' component={LoginForm} />
+        </Route>
       </Router>
     </ApolloProvider>
   );
