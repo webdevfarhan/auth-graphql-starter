8f0cf43b790c36fa8b16f33931dc1b4111e733a2
Farhan <frhnmdni@gmail.com>
2019-09-06 01:45:49 +0530

    dashboard and route

--- client/index.js
+++ client/index.js
@@ -7,6 +7,7 @@ import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import SignupForm from './components/SignupForm';
 import LoginForm from './components/LoginForm';
+import Dashboard from './components/Dashboard';
 
 const client = new ApolloClient({
   dataIdFromObject: o => o.id
@@ -19,6 +20,7 @@ const Root = () => {
         <Route path='/' component={App}>
           <Route path='/login' component={LoginForm} />
           <Route path='/signup' component={SignupForm} />
+          <Route path='/dashboard' component={Dashboard} />
         </Route>
       </Router>
     </ApolloProvider>
