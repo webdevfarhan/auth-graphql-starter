9505ac606ec982c6781c4b089a81dee21f8c8406
Farhan <frhnmdni@gmail.com>
2019-09-06 02:15:14 +0530

    require auth

---
+++ client/components/requireAuth.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import currentUserQuery from '../queries/CurrentUser';
+
+class RequireAuth extends Component {
+  componentDidMount() {
+    if (!this.props.data.loading && !this.props.data.user) {
+      hashHistory.push('/login');
+    }
+  }
+}
+
+graphql(currentUserQuery)(RequireAuth);
