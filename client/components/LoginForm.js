f77bbc7041023af3c69c5d0f813d823098db2761
Farhan <frhnmdni@gmail.com>
2019-09-05 13:16:46 +0530

    fixing the login process

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
+import { hashHistory } from 'react-router';
+
 import AuthForm from './AuthForm';
 import mutation from '../mutations/Login';
 import query from '../queries/CurrentUser';
@@ -8,6 +10,12 @@ class LoginForm extends Component {
   constructor(props) {
     super(props);
     this.state = { errors: [] };
+  }
+
+  componentWillUpdate(nextProps) {
+    if (!this.props.data.user && nextProps.data.user) {
+      hashHistory.push('/dashboard');
+    }
   }
 
   onSubmit({ email, password }) {
@@ -32,4 +40,4 @@ class LoginForm extends Component {
   }
 }
 
-export default graphql(mutation)(LoginForm);
+export default graphql(query)(graphql(mutation)(LoginForm));
