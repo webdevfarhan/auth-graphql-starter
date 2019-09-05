681da726a2f694825abbe1d8edb118d5a1204c3c
Farhan <frhnmdni@gmail.com>
2019-09-06 02:44:46 +0530

    fixing requireAuth

--- client/components/requireAuth.js
+++ client/components/requireAuth.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import currentUserQuery from '../queries/CurrentUser';
+import { hashHistory } from 'react-router';
 
 export default WrappedComponent => {
   class RequireAuth extends Component {
-    componentDidMount() {
-      if (!this.props.data.loading && !this.props.data.user) {
+    componentWillUpdate(nextProps) {
+      if (!nextProps.data.loading && !nextProps.data.user) {
         hashHistory.push('/login');
       }
     }
